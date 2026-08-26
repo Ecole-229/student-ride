7ca4af3de258fdbe89db0088f370324bcf3d0443
Aubin SOSSOU <aubinsossoua@gmail.com>
2026-08-26 22:06:56 +0100

    feat: mise en place de l'authentification (register, login, logout) et leurs routes

--- student_ride_back/app/Http/Controllers/Api/UserController.php
+++ student_ride_back/app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,92 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string',
+            'phone' => 'required|string|max:20',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'errors' => $validate->errors(),
+                'message' => 'Les données fournies sont invalides.'
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone' => $request->phone,
+        ]);
+        return response()->json([
+            'message' => 'Inscription réussie.',
+            'data' => $user,
+        ], 201);
+    }
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'errors' => $validate->errors(),
+                'message' => 'Les données fournies sont invalides.'
+            ], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Aucun utilisateur enregistré avec cet email.',
+            ], 404);
+
+        }
+        if ($user && !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Mot de passe incorrect.',
+            ], 401);
+        }
+
+        if ($user->statut === 'bloque') {
+
+            return response()->json([
+                'message' => 'Votre compte est bloqué.',
+                'data' => $user,
+            ], 403);
+        }
+        if ($user && Hash::check($request->password, $user->password) && $user->statut !== 'bloque') {
+
+            $accessToken = $user->createToken('authToken')->accessToken;
+            $refreshToken = $user->createToken('refreshToken')->accessToken;
+
+            return response()->json([
+                'message' => 'Connexion réussie.',
+                'data' => $user,
+                'access_token' => $accessToken,
+                'refresh_token' => $refreshToken,
+            ], 200);
+        }
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Déconnexion réussie.',
+        ], 200);
+    }
 }
--- student_ride_back/routes/api.php
+++ student_ride_back/routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::prefix('/v1/auth')->group(function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:api');
+});
